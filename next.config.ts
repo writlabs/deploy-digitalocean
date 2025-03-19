2b0695f0ff30037b0635feff78831eda448ca547
writlabs <87889375+writlabs@users.noreply.github.com>
2025-03-19 22:11:18 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from 'next';
 
+// Log environment variables (for debugging)
+console.log('Environment Variables:', process.env);
+
 const nextConfig: NextConfig = {
   output: 'standalone',
 };
